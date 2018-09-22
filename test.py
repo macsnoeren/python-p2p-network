190490c4be50ee8ce8f2d5ec85d716743d77443e
Maurice Snoeren <mac.snoeren@avans.nl>
2018-09-22 02:04:13 +0200

    Added the events.

--- test.py
+++ test.py
@@ -16,30 +16,33 @@ node2 = None
 
 def callback_node_event(event, node, other, data):
     if ( event == "NODEINBOUNDCLOSED" ):
-        print("NODE: " + event + "\n")
+        print("NODE (" + node.getName() + "): " + event + "\n")
 
     elif ( event == "NODEOUTBOUNDCLOSED" ):
-        print("NODE: " + event + "\n")
+        print("NODE (" + node.getName() + "): " + event + "\n")
 
     elif ( event == "CONNECTEDWITHNODE" ):
-        print("NODE: " + event + "\n")
+        print("NODE (" + node.getName() + "): " + event + "\n")
 
     elif ( event == "NODECONNECTED" ):
-        print("NODE: " + event + "\n")
+        print("NODE (" + node.getName() + "): " + event + "\n")
 
     elif ( event == "NODEMESSAGE" ):
-        print("NODE: " + event + ": " + str(data) + "\n")
+        print("NODE (" + node.getName() + "): " + event + ": " + str(data) + "\n")
 
     else:
-        print("NODE: Event is not known: " + event)
+        print("NODE (" + node.getName() + "): Event is not known " + event + "\n")
 
 node1 = Node('localhost', 10000, callback_node_event)
 node2 = Node('localhost', 20000, callback_node_event)
+node3 = Node('localhost', 30000, callback_node_event)
 
 node1.start()
 node2.start()
+node3.start()
 
 node1.connect_with_node('localhost', 20000)
+node3.connect_with_node('localhost', 10000)
 
 time.sleep(2)
 
@@ -49,7 +52,6 @@ node1.send_to_nodes({"test": "ha", "ni": "hao"})
 
 while True:
     node1.send_to_nodes({"node": "Node 1", "test": "ha", "ni": "hao"})
-    node2.send_to_nodes({"node": "Node 2", "test": "ha", "ni": "hao"})
     time.sleep(1)
 
 node1.stop()
